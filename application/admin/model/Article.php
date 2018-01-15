4bfb20e84171604d01a77e3c458f3f0c9f85c5ee
ywb1995 <1540601646@qq.com>
2018-01-16 01:12:09 +0800

    文章插入的前置事件，上传图片，并且上传失败时中断插入操作的执行

--- application/admin/model/Article.php
+++ application/admin/model/Article.php
@@ -5,5 +5,22 @@ use think\Model;
 class Article extends Model{
     protected $table = 'tp_admin_article';
 
+    //模型事件
+    protected static function init()
+    {
+        Article::event('before_insert', function ($data) {
+            //接收文件
+            $file = request()->file('image');
+            if($file){
+                $info = $file->validate(['size'=>1024*1024*1024*50,'ext'=>'jpg,png,gif'])->rule('')->move(ROOT_PATH . 'public'.DS."upload".DS.date("YmdHi"));
+                if(!$info){
+                    $data->error = $file->getError();
+                    //return false 可以中断插入操作的执行
+                    return false;
+                }
+                $data['image'] = DS."upload".DS.date("YmdHi").DS.$info->getSaveName();
+            }
+        });
+    }
 
 }
